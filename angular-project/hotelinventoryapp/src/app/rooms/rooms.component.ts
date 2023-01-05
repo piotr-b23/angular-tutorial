372a48cc60cc7eb8432b362a2963d1dff137b733
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-01-05 16:54:33 +0100

    ngif ngfor learning

--- angular-project/hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ angular-project/hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,15 +1,47 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent {
-hotelName = "Hilton Hotel."
-numberOfRooms = 100;
-hideRooms = false;
-toggle(){
-  this.hideRooms = ! this.hideRooms;
-}
+  hotelName = 'Hilton Hotel.';
+  numberOfRooms = 100;
+  hideRooms = false;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 15,
+  };
+
+  roomlist: RoomList[] = [
+    {
+      roomType: 'Deluxe Room',
+      amenities: 'Tv, Kitchen',
+      price: 500,
+      photos: 'http://ttt.com/400/200/sports/',
+      checkinTime: new Date('11-JUL-2019'),
+    },
+    {
+      roomType: 'Premium Room',
+      amenities: 'Tv, Kitchen',
+      price: 1500,
+      photos: 'http://ttt.com/400/200/sports/',
+      checkinTime: new Date('11-JUL-2019'),
+    },
+    {
+      roomType: 'Premium Room',
+      amenities: 'Tv, Kitchen',
+      price: 2500,
+      photos: 'http://ttt.com/400/200/sports/',
+      checkinTime: new Date('11-JUL-2019'),
+    },
+  ];
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
 }
