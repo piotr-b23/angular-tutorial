02c26d78316351012f4345d0f4235c0b521d4d95
Piotr Bobeł <piotr.bobel23@gmail.com>
2023-01-08 13:26:08 +0100

    tasks implementation

---
+++ angular-task/src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+import  {Task} from '../../task';
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.scss']
+})
+export class TaskItemComponent {
+@Input() task!: Task;
+}
---
+++ angular-task/src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {Task} from '../../task';
+import {TASKS} from '../../mock-task';
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.scss']
+})
+export class TasksComponent {
+
+  tasks: Task[] = TASKS;
+
+}
