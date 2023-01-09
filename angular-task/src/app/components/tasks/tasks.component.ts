ac9abd711da9677812a4fb21bea239d37d60b4da
Piotr Bobeł <piotr.bobel23@gmail.com>
2023-01-09 22:21:03 +0100

    adding task work

--- angular-task/src/app/components/tasks/tasks.component.ts
+++ angular-task/src/app/components/tasks/tasks.component.ts
@@ -29,4 +29,8 @@ export class TasksComponent {
     this.taskService.updateTaskReminder(task).subscribe();  
   }
 
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => (this.tasks.push(task)));
+  }
+
 }
