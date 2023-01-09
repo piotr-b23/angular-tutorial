59abbf89a3d8c5be5da4f0f013544561d99d7dc7
Piotr Bobeł <piotr.bobel23@gmail.com>
2023-01-09 22:58:27 +0100

    finished app

--- angular-task/src/app/app.module.ts
+++ angular-task/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {RouterModule, Routes} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
@@ -10,6 +12,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {HttpClientModule} from '@angular/common/http';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,13 +26,16 @@ import { FormsModule } from '@angular/forms';
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes,{enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-task/src/app/components/header/header.component.ts
+++ angular-task/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {UiService} from '../../services/ui.service';
 import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,11 +13,14 @@ export class HeaderComponent {
   showAddTask?: boolean;
   subscription?: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
   }
 
   toggleAddTask(){
     this.uiService.toggleAddTask();
+  }
+  hasRoute(route: String) {
+    return this.router.url === route;
   }
 }
