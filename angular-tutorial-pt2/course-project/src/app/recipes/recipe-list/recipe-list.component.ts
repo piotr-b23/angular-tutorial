878d51e0a1b39e9bc3d281a73220062b7c46c949
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-01-18 17:27:22 +0100

    recipes displaying

--- angular-tutorial-pt2/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-tutorial-pt2/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent {
+  recipes: Array<Recipe> = [
+    new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg'),
+    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg'),
+    new Recipe('A Test Recipe 3', 'This is simply a test 3', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg')
+  ];
 
 }
