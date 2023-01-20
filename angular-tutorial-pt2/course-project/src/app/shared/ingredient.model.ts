fa7c853c01ec5784e4038ad4aae376d305ec73ed
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-01-20 14:21:17 +0100

    basics project element created

---
+++ angular-tutorial-pt2/course-project/src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingredient {
+    public name: string;
+    public ammount: number;
+
+    constructor(name: string, ammount: number) {
+        this.name = name;
+        this.ammount = ammount;
+    }
+}
