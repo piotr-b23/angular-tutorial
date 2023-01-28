363bc961e2624ab1e2e6231cff7c1407146fde39
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-01-28 14:11:27 +0100

    services

--- angular-tutorial-pt2/services-start/src/app/new-account/new-account.component.ts
+++ angular-tutorial-pt2/services-start/src/app/new-account/new-account.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountService } from '../account.service';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
@@ -6,13 +8,18 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService,
+    private accountService: AccountService) {
+      this.accountService.statusUpdated.subscribe(
+        (status: string) => {
+          alert('New status '+ status);
+        }
+      );
+    }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountService.addAccount(accountName, accountStatus);
+ //   this.loggingService.logStatusChange(accountStatus);
   }
 }
