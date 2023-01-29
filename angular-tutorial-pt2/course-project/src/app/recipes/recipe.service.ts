fc60703c7aa0a0be10bf8cbba7ecdf036036fb88
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-01-29 14:06:13 +0100

    services to add multiple ingredients

--- angular-tutorial-pt2/course-project/src/app/recipes/recipe.service.ts
+++ angular-tutorial-pt2/course-project/src/app/recipes/recipe.service.ts
@@ -1,22 +1,43 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
-
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Array<Recipe> = [
-    new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg'),
-    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg'),
-    new Recipe('A Test Recipe 3', 'This is simply a test 3', 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg')
+    new Recipe(
+      'A Test Recipe 1',
+      'This is simply a test 1',
+      'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'A Test Recipe 2',
+      'This is simply a test 2',
+      'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg',
+      [new Ingredient('Buns', 2), new Ingredient('French Fries', 25)]
+    ),
+    new Recipe(
+      'A Test Recipe 3',
+      'This is simply a test 3',
+      'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/newscms/2021_32/1759222/ratatouille-mc-main-210809-v2.jpeg',
+      [new Ingredient('Meat', 3), new Ingredient('French Fries', 123)]
+    ),
   ];
 
-  getRecipes(){
+  getRecipes() {
     return this.recipes.slice();
   }
 
-  constructor() { }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+
+  }
+
+  constructor(private slService: ShoppingListService) {}
 }
