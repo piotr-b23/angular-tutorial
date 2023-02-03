c574bd50852399cd9ed8ed5368b7358dc362ac12
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-03 22:24:48 +0100

    observab;es complete

--- angular-tutorial-pt2/obs-01-start/src/app/app.component.ts
+++ angular-tutorial-pt2/obs-01-start/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs-compat';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  subscribtion: Subscription;
+  constructor(private userService: UserService) {}
+  ngOnDestroy(): void {
+    this.subscribtion.unsubscribe();
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.subscribtion = this.userService.activatedEmitter.subscribe(didActivate => {
+      this.userActivated = didActivate;
+    })
+  }
+
+  
 }
