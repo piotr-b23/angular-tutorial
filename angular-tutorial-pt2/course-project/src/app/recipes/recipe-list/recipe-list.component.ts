0dffeb1c36e95e17654e79f65b76689e4036f399
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-03 16:13:49 +0100

    course project routing done

--- angular-tutorial-pt2/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-tutorial-pt2/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { outputAst } from '@angular/compiler';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -12,9 +13,13 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Array<Recipe> = [];
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 }
