2e190fae698ad4d78816b6ebabc4410fb37d1520
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-08 20:58:10 +0100

    reactive forms assigment done

--- angular-tutorial-pt2/forms-reactive-assignment-start/src/app/app.component.ts
+++ angular-tutorial-pt2/forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null,[Validators.required, this.invalidProjectName.bind(this)],this.asyncInvalidProjectName),
+      'email': new FormControl(null,[Validators.required, Validators.email]),
+      'status': new FormControl('critical')
+      
+      
+    })
+  }
+
+  invalidProjectName(control: FormControl): {[s: string]: boolean} {
+    if(control.value === 'test') {
+      return {'invalidProjectName': true};
+    } else {
+      return null;
+    }
+  }
+
+  asyncInvalidProjectName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        if (control.value === 'tesa') {
+          resolve({'invalidProjectNameAsync': true});
+        } else {
+          resolve(null);
+        }
+      },2000)
+    })
+    return promise;
+  }
+
+
+  onSubmit(){
+    console.log(this.projectForm.value);
+  }
+
 }
