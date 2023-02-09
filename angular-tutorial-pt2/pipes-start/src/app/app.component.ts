5982b8f33be4bd1bb6baa59b54058cbc144d5c87
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-09 17:59:39 +0100

    pipes done

--- angular-tutorial-pt2/pipes-start/src/app/app.component.ts
+++ angular-tutorial-pt2/pipes-start/src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      resolve('stable');
+    },2000)
+  });
   servers = [
     {
       instanceType: 'medium',
@@ -39,4 +44,15 @@ export class AppComponent {
       'list-group-item-danger': server.status === 'critical'
     };
   }
+
+  onAddServer(){
+    this.servers.push({
+      instanceType: 'small',
+      name: 'Development Server Test',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    });
+  }
+
+  filteredStatus = '';
 }
