2a5066916543f13c6cfec512a23d82b1394db852
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-13 16:35:56 +0100

    http part done

--- angular-tutorial-pt2/http-01-start/src/app/logging-interceptor.service.ts
+++ angular-tutorial-pt2/http-01-start/src/app/logging-interceptor.service.ts
@@ -3,18 +3,19 @@ import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
 
 
-export class AuthInterceptorService implements HttpInterceptor{
+export class LoggingInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log('Req is on its way');
+    console.log('Outgoing request');
     console.log(req.url);
-    const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
-    return next.handle(modifiedRequest).pipe(tap(event => {
-      if (event.type === HttpEventType.Response) {
-        console.log('Response arrived');
+    console.log(req.headers);
+    return next.handle(req).pipe(tap(event => {
+      if ( event.type === HttpEventType.Response) {
+        console.log('Incomming response');
         console.log(event.body);
       }
     }));
   }
+
   constructor() { }
 }
