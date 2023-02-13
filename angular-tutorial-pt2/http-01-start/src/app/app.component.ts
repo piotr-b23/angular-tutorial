160c60127eee2a1670d8972ffedeb2fe6d4f3a26
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-02-13 15:18:39 +0100

    error handling done

--- angular-tutorial-pt2/http-01-start/src/app/app.component.ts
+++ angular-tutorial-pt2/http-01-start/src/app/app.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
   isFetching = false;
+  error = null;
+  private errorSub: Subscription;
 
   constructor(private http: HttpClient, private postService: PostsService) {}
 
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
+  }
+
   ngOnInit() {
+    this.errorSub = this.postService.error.subscribe(error => (this.error = error));
     this.isFetching = true;
     this.postService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, error => {
+      this.error = error.message;
     });
   }
 
@@ -29,15 +39,20 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
+
     this.isFetching = true;
     this.postService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
       this.loadedPosts = posts;
+    }, error => {
+      this.error = error.message;
     });
   }
 
   onClearPosts() {
-    // Send Http request
+    this.postService.deletePosts().subscribe(()=>{
+      this.loadedPosts = [];
+    });
   }
 
 }
--- angular-tutorial-pt2/http-01-start/src/app/posts.service.ts
+++ angular-tutorial-pt2/http-01-start/src/app/posts.service.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { Post } from './post.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  error = new Subject<string>();
 
   createAndStorePost(title: string, content: string) {
     const postData: Post = {title: title, content: content};
     this.http.post<{name: string}>('https://ng-complete-guide-4af0f-default-rtdb.firebaseio.com/posts.json', postData).subscribe(responseData => {
       console.log(responseData);
+    }, error => {
+      this.error.next(error.message);
     });
   }
 
@@ -26,8 +30,14 @@ export class PostsService {
         }
       }
       return postsArray;
+    }), catchError(errorRes => {
+      return throwError(errorRes);
     }))
 
+  }
+
+  deletePosts(){
+   return this.http.delete('https://ng-complete-guide-4af0f-default-rtdb.firebaseio.com/posts.json');
   }
 
   constructor(private http: HttpClient) { 
