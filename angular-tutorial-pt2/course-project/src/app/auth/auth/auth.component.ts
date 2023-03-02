13c6a2d2f472bf0f55266ac8352657c7427621b6
piotrekkk23 <gppiotrekgp23@gmail.com>
2023-03-02 10:55:46 +0100

    added form validation

--- angular-tutorial-pt2/course-project/src/app/auth/auth/auth.component.ts
+++ angular-tutorial-pt2/course-project/src/app/auth/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent {
+  isLoginMode = true;
+
+  onSwitchMode(){
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form.value);
+    form.reset();
+  }
 
 }
